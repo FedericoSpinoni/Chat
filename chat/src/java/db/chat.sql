4db389cdaa8a167dd228aaf8d62b009416517c16
michaelMattanza <michael.mattanza@gmail.com>
2018-03-18 22:25:48 +0100

    Aggiunta di hibernate e database nel progetto

---
+++ chat/src/java/db/chat.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 16, 2018 alle 11:29
+-- Versione del server: 10.1.26-MariaDB
+-- Versione PHP: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_sender` int(10) UNSIGNED NOT NULL,
+  `id_receiver` int(10) UNSIGNED NOT NULL,
+  `id_message` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `chat`
+--
+
+INSERT INTO `chat` (`id`, `id_sender`, `id_receiver`, `id_message`) VALUES
+(1, 5, 6, 5),
+(2, 6, 5, 6),
+(5, 6, 5, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `message` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `message`
+--
+
+INSERT INTO `message` (`id`, `message`) VALUES
+(5, 'ciao sono federico'),
+(6, 'ciao sono michael'),
+(9, 'come stai?');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `status` enum('online','offline') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `status`) VALUES
+(5, 'fedespino', '1234', 'offline'),
+(6, 'biancomichael', '5678', 'offline');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sender` (`id_sender`),
+  ADD KEY `id_receiver` (`id_receiver`),
+  ADD KEY `id_message` (`id_message`);
+
+--
+-- Indici per le tabelle `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT per la tabella `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT per la tabella `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`id_message`) REFERENCES `message` (`id`),
+  ADD CONSTRAINT `chat_ibfk_3` FOREIGN KEY (`id_receiver`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

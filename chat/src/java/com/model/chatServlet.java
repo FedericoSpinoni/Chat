14c7eaf64d68537777ac8480e38a908971ddf38b
michaelMattanza <michael.mattanza@gmail.com>
2018-04-13 16:58:56 +0200

    Sistemate le entità

---
+++ chat/src/java/com/model/chatServlet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import com.entity.Chat;
+import com.entity.Message;
+import com.entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author mike
+ */
+@WebServlet(name = "chatServlet", urlPatterns = {"/chatServlet"})
+public class chatServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SessionFactory factory = session.getSessionFactory();
+        Session s = factory.openSession(); // creo una sessione e la avvio
+        
+        List<Chat> users= s.createQuery("FROM Chat").list(); 
+        List<Message> usrs= s.createQuery("FROM Message").list(); 
+    }
+}
